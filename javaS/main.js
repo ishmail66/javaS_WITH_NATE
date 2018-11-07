5b04379e8f272336abd0dc9b321c4a9aca35bc56
ishmail66 <ishmail66@gmail.com>
2018-11-07 11:56:33 +0200

    first lesson on JavaS

---
+++ javaS/main.js
@@ -0,0 +1,94 @@
+'use strict';
+    // the 'use strict' directive switches the engine to the "modern" mode, changing the behavior of some built-in features
+
+    // variables are used to store this information.
+
+    // a variable is a " names storage" for data. we can use variables to store data.
+
+    // to create a variable in JavaScript, we need to use the let or var keyword.
+
+    var message
+
+    // assignment operator =
+
+    // we add a string ''
+    // a string in javaScript is a set of characters surrounded by quotations marks.
+
+    var message = 'good morming';
+        // message='how are you today?';
+    alert(message);
+    // creating promp
+
+    var name = prompt("enter your first name:");
+alert("hello, " +name); 
+
+    // var name ='nate';
+    //      age= 43;
+    //      gender='female';
+    //      type='nafees'
+    //      console.log(name);
+
+
+    // *******************************************************************************
+    // *******************************************************************************
+
+    // always remember to close a atring witht the same type of quotations marks you started it with.
+     
+    // operators:
+
+    // +addition
+    // -subtraction
+    // /division
+    // *multiplication
+
+    // the + operator can be applied to two strings values. This will join two strings together, otherwise called concatenations.
+
+    // in the concole "hey" + " there."
+
+    // choose meaningful names
+    // dont use reserves words 
+    // follow conventions
+
+    // https:developer.mozilla.org/en-US/docs/Web/JavaScript/reference/Lexical_grammar#Keywords
+
+    // begin
+    //          get out the rice cooker
+    //          fill it with rice
+    //          fill it with water
+    // end
+
+    // up untill now, all the code in our programes has been executed chronological.lets enrich our code by adding conditional execution!
+
+    // whats a condition?
+
+    // contatination
+
+    var age=23;
+    var name="ishmail"
+    var surname="sadien"
+    var gender="male"
+    var institute=" LifeChoices"
+    var studies="coding"
+    var address="3 adrian rd ottery"
+
+
+    console.log("My name is " + name + " " + surname + " I am  a " + gender + " I live in " + address + " I currently study " + studies + " at the " + institute + ' in lansdowne.')
+    console.log("first name is " + name + " " + surname)
+
+    var name= "feroza";
+
+    console.log("my mothers name is " + name)
+
+    var name="zainab"
+
+    console.log("i love " + name)
+
+    var name="yelp"
+
+    console.log("i am nafees i need " + name)
+
+    var name="jason"
+    var car="silver bullet"
+    var size=11;
+
+    console.log("my name is " + name + " i drive a " + car + " i wear a size " + size)
